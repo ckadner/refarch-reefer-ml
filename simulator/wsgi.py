47a8bef87bf3baf385d454896813820fa26a633c
Christian Kadner <ckadner@us.ibm.com>
2019-11-12 11:42:37 -0800

    Add reefer simulator maintenance endpoint (hack)

--- simulator/wsgi.py
+++ simulator/wsgi.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from infrastructure.MetricsEventsProducer import MetricsEventsProducer 
 from domain.reefer_simulator import ReeferSimulator
 
-VERSION = "Reefer Container simulator v0.0.4"
+VERSION = "Reefer Container simulator v0.0.6 10/09"
 application = Flask(__name__)
 
 metricsProducer = MetricsEventsProducer()
@@ -22,16 +22,21 @@ def runSimulator():
     control = request.json
     simulator = ReeferSimulator()
     if control["simulation"] == ReeferSimulator.SIMUL_POWEROFF:
-        metrics=simulator.generatePowerOffTuples(control["containerID"],int(control["nb_of_records"]),float(control["good_temperature"]))
-    elif  control["simulation"]  == ReeferSimulator.SIMUL_CO2:
-        metrics=simulator.generateCo2Tuples(control["containerID"],int(control["nb_of_records"]),float(control["good_temperature"]))
+        metrics=simulator.generatePowerOffTuples(control["containerID"],int(control["nb_of_records"]),control["product_id"])
+    elif control["simulation"] == ReeferSimulator.SIMUL_CO2:
+        metrics=simulator.generateCo2Tuples(control["containerID"],int(control["nb_of_records"]),control["product_id"])
+    elif control["simulation"] == ReeferSimulator.SIMUL_O2:
+        metrics=simulator.generateO2Tuples(control["containerID"],int(control["nb_of_records"]),control["product_id"])
+    elif control["simulation"] == ReeferSimulator.MAINTENANCE:
+        print("sending 1 maintenance record")
+        metrics = [f"('{control['containerID']}', '2019-10-09T23:51:20.137127000', '{control['product_id']}', 15.21328744, 3.0, 19.84017345, 3.442838, 0.84450906, 2, 6, 10.62306835, 79.39353212, 80.58489227, 14.92076336, False, False, False, 6)",]
     else:
         return "Wrong simulation controller data"
     
     for metric in metrics:
         evt = {"containerID": control["containerID"],
                 "timestamp": str(metric[0]),
-                "type":"ContainerMetric",
+                "type":"ReeferTelemetries",
                 "payload": str(metric)}
         metricsProducer.publishEvent(evt,"containerID")
     return "Simulation started"
@@ -40,4 +45,3 @@ def runSimulator():
 if __name__ == "__main__":
     print(VERSION)
     application.run()
-    
